0fcccd055fafe1dd29d74caaeee360d82932f350
Piwero <72553098+Piwero@users.noreply.github.com>
2020-11-08 10:48:59 +0000

    Update ex05_03.py

--- ex05_03.py
+++ ex05_03.py
@@ -10,3 +10,14 @@ is_triangle(8,2,2)
 
 #test NO
 is_triangle(9,2,2)
+
+#new func with inputs
+def input_is_triangle():
+	a = int(input("Enter size of the side a: "))
+	b = int(input("Enter size of the side b: "))
+	c = int(input("Enter size of the side c: "))
+	return is_triangle(a,b,c)
+
+#test with inputs
+input_is_triangle()
+
